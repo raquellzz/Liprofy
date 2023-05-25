9f16a3ed20c3c04356c2e96b1e915e9c95715039
turevs <artur.rp2001@gmail.com>
2023-05-25 16:07:13 -0300

    No.h

---
+++ novo/No.h
@@ -0,0 +1,36 @@
+#include <iostream>
+
+#pragma once
+
+template <typename T>
+class No
+{
+public:
+private:
+    T valor;
+    No<T> *proximo;
+
+public:
+    // Métodos especiais
+    // Cstrc.
+    No(T valor, No<T> &proximo)
+    {
+        this->valor = valor;
+        this->proximo = proximo;
+    }
+    // Dstrc.
+    ~No() { delete valor; }
+    // Operadores
+    friend operator=(No<T> &no)
+    {
+        this->valor = no.getValor();
+        this->proximo = no.getProximo();
+    }
+    friend operator==(No<T> &no) { return this->valor == no.getValor(); }
+    // sets
+    void setProximo(No<T> *proximo) { this->proximo = proximo; }
+    void setValor(T valor) { this->valor = valor; }
+    // gets
+    T getValor() { return valor; }
+    No<T> *getProximo() { return proximo; }
+};
